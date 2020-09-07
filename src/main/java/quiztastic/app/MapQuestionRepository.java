ac84d612b9f64d5578ce1f40e5bb00ec32e3386e
Emil Elkjær Nielsen <emil@visiontrade.dk>
2020-09-07 16:31:51 +0200

    MapQuestionRepository.java updated

--- src/main/java/quiztastic/app/MapQuestionRepository.java
+++ src/main/java/quiztastic/app/MapQuestionRepository.java
@@ -5,10 +5,12 @@ import quiztastic.core.Question;
 import quiztastic.domain.QuestionRepository;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MapQuestionRepository implements QuestionRepository {
     private final HashMap<Category, List<Question>> questionsByCategory;
@@ -18,19 +20,30 @@ public class MapQuestionRepository implements QuestionRepository {
     }
 
     public static MapQuestionRepository fromQuestionReader(QuestionReader reader) throws IOException, ParseException {
-        reader.readQuestion();
-        //throw new UnsupportedOperationException("Not implemented yet!");
-        return new MapQuestionRepository(new HashMap<>());
+        Question q;
+        HashMap<Category, List<Question>> questionsByCategory = new HashMap<>();
+        while((q=reader.readQuestion()) != null){
+            List<Question> current = questionsByCategory.get(q.getCategory());
+
+            if (current == null) {
+                current = new ArrayList<>();
+
+                questionsByCategory.put(q.getCategory(), current);
+            }
+            current.add(q);
+        }
+
+        return new MapQuestionRepository(questionsByCategory);
     }
 
     @Override
     public List<Category> getCategories() {
-        throw new UnsupportedOperationException("Not implemented yet!");
+        return List.copyOf(questionsByCategory.keySet());
     }
 
     @Override
     public List<Question> getQuestionsWithCategory(Category category) {
-        throw new UnsupportedOperationException("Not implemented yet!");
+        return List.copyOf(questionsByCategory.get(category));
     }
 
     @Override
